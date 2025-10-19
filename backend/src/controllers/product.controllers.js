b359c30bccb3bdd8fca328cbc9c6a58c0afce010
mark-j-yadav <yadavjitendre359>
2025-10-19 12:42:34 +0530

    product controllers file added

---
+++ backend/src/controllers/product.controllers.js
@@ -0,0 +1,215 @@
+import mongoose from "mongoose";
+import { asyncHandler } from "../helpers/asyncHandler.js";
+import { ApiError } from "../helpers/ApiError.js";
+import { Product } from "../models/product.model.js";
+import { ApiResponse } from "../helpers/ApiResponse";
+
+const createProdcut = asyncHandler(async(req, res)=> {
+    try {
+          const { name, description, price, category, quantity, brand } = req.fields;
+          switch(true){
+            case !name: 
+                throw new ApiError(400, "name is required");
+            case !description:
+                throw new ApiError(400, "description is rquired");
+            case !price:
+                throw new ApiError(400, "price fields is required");
+            case !category:
+                throw new ApiError(400, "category is requied fields");
+            case !quantity :
+                throw new ApiError(400, "quantity is required fields");
+            case !brand:
+                throw new ApiError(400, "brand is required fields")
+
+          }
+
+          const product = new Product({...req.fields});
+          await product.save()
+          res.status(200).json(
+            new ApiResponse(201, product, "product was sussfully added")
+          )
+        
+    } catch (error) {
+         throw new ApiError(`error product ${error}`)
+    }
+})
+
+const upadateProduct = asyncHandler(async(req, res) => {
+     try {
+         const { name, description, price, category, quantity, brand } = req.fields;
+         switch(true){
+               case !name: 
+                   throw new ApiError(400, "name is required");
+               case !description:
+                   throw new ApiError(400, "description is rquired");
+               case !price:
+                   throw new ApiError(400, "price fields is required");
+               case !category:
+                   throw new ApiError(400, "category is requied fields");
+               case !quantity :
+                   throw new ApiError(400, "quantity is required fields");
+               case !brand:
+                   throw new ApiError(400, "brand is required fields")
+   
+             }
+       const prodcut = await Product.findByIdAndUpdate(
+           req.params.id,
+           {...req.fields},
+           {new:true}
+       )
+       await prodcut.save()
+       res.status(200).json(
+        new ApiResponse(201, prodcut, "product upadate was sussfully")
+       )
+       
+     } catch (error) {
+        
+        console.log(error)
+        throw new ApiError(400, "product upadate error ")
+     }
+})
+
+
+const removeProduct = asyncHandler(async(req,res) => {
+    try {
+        const product  = await Product.findByIdAndDelete(req.params.id);
+        res.json(product)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:"server error"})
+    }
+})
+const fetchProducts = asyncHandler(async (req, res) => {
+  try {
+    const pageSize = 6;
+
+    const keyword = req.query.keyword
+      ? {
+          name: {
+            $regex: req.query.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword }).limit(pageSize);
+
+    res.json({
+      products,
+      page: 1,
+      pages: Math.ceil(count / pageSize),
+      hasMore: false,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+const fetchProductById = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: "Product not found" });
+  }
+});
+const fetchAllProducts = asyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find({})
+      .populate("category")
+      .limit(12)
+      .sort({ createAt: -1 });
+
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+
+const productReview = asyncHandler(async(req,res) => {
+     try {
+    const { rating, comment } = req.body;
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      const alreadyReviewed = product.reviews.find(
+        (r) => r.user.toString() === req.user._id.toString()
+      );
+
+      if (alreadyReviewed) {
+        res.status(400);
+        throw new Error("Product already reviewed");
+      }
+
+      const review = {
+        name: req.user.username,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+      };
+
+      product.reviews.push(review);
+
+      product.numReviews = product.reviews.length;
+
+      product.rating =
+        product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+        product.reviews.length;
+
+      await product.save();
+      res.status(201).json({ message: "Review added" });
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error.message);
+  }
+})
+const fetchTopProducts = asyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find({}).sort({ rating: -1 }).limit(4);
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error.message);
+  }
+});
+
+const fetchNewProducts = asyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find().sort({ _id: -1 }).limit(5);
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error.message);
+  }
+});
+
+const filterProducts = asyncHandler(async (req, res) => {
+  try {
+    const { checked, radio } = req.body;
+
+    let args = {};
+    if (checked.length > 0) args.category = checked;
+    if (radio.length) args.price = { $gte: radio[0], $lte: radio[1] };
+
+    const products = await Product.find(args);
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+export {createProdcut, upadateProduct, removeProduct, fetchProducts,fetchProductById, fetchAllProducts,productReview,fetchTopProducts, fetchNewProducts, filterProducts}
