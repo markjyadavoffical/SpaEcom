c02295c6511a8d98368a4fc7f4ead38e1bc61a6c
mark-j-yadav <yadavjitendre359>
2025-10-21 13:45:58 +0530

    app.js backend file added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path"
 
 const app = express()
 app.use(cors({
@@ -12,4 +13,25 @@ app.use(express.json({limit:"16kb"}))
 app.use(cookieParser())
 
 
+
+//router define
+import userRotuer from "./src/routes/uload.routes.js";
+import productRouter from "./src/routes/product.routes.js";
+import orderRouter from "./src/routes/order.routes.js";
+import categoryRouter from "./src/routes/category.routes.js";
+import uplaodRouter from "./src/routes/uload.routes.js";
+
+
+
+app.use("/api/user",userRotuer);
+app.use("/api/product",productRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/category",categoryRouter);
+app.use("api/uplaod", uplaodRouter);
+
+
+const __dirname = path.resolve()
+app.use("/upolads", express.static(path.join(__dirname + "/upolads")))
+
 export {app}
+
